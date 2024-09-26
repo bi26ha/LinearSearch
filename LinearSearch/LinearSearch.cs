942c7dc5d6880148ed34cf58baa7aac3bbdfaa8f
bi26ha <bi26ha@FOT-CELLC12>
2024-09-26 12:23:22 +0100

    Add project files.

---
+++ LinearSearch/LinearSearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    internal class LinearSearch
+    {
+        public static int Perform(double SearchValue, List<double> ListToSearch)
+        {
+            for (int i = 0; i < ListToSearch.Count; i++)
+            {
+                if (ListToSearch[i] > SearchValue)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static int Perform(string SearchValue, List<string> listToSearch)
+        {
+            for (int i = 0; i < listToSearch.Count; i++)
+            {
+                if (string.Compare(listToSearch[i], SearchValue) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+    }
+}
---
+++ LinearSearch/program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearSearch
+{
+    internal class program
+    {
+        static Random randomGenerator = new Random();
+
+        static void Main(string[] args)
+        {
+            List<double> listofDoubles = new List<double>();
+
+            PopulateListWithRandomDoubles(ref listofDoubles, 10);
+            PrintList(listofDoubles);
+
+            RequestSearch(listofDoubles);
+            PrintList(listofDoubles);
+
+            Console.ReadKey();
+        }
+
+        static void RequestSearch(List<double> list)
+        {
+            Double searchValue;
+            Console.WriteLine("What value would would you like to search for?");
+
+            if (Double.TryParse(Console.ReadLine(), out searchValue))
+            {
+                int index = BinarySearch.Perform(searchValue, list);
+
+                if (index < 0)
+                {
+
+                    Console.WriteLine("NOT FOUND");
+
+                }
+                else
+                {
+                    Console.WriteLine("Found at: " + index);
+                }
+            }
+        }
+
+        static void PopulateListWithRandomDoubles(ref List<double> list, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00"));
+                list.Add(twoDigitDouble);
+            }
+
+            list.Sort();
+        }
+
+        static void PrintList(List<double> list)
+        {
+
+            Console.WriteLine("\nLIST PRINT \n");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(" " + list[i].ToString());
+            }
+            Console.WriteLine("\nEND \n");
+
+        }
+
+    }
+}
+
+    
+  
+
+    
+
+    
+
+
+
